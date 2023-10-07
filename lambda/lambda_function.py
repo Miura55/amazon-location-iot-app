a0e676b3dbb296f9f5057624897c7d4b1a210167
Miura55 <acordion.piano@gmail.com>
2023-10-07 16:16:12 +0900

    Add Lambda function

---
+++ lambda/lambda_function.py
@@ -0,0 +1,36 @@
+import json
+import boto3
+
+sns = boto3.client('sns')
+iot = boto3.client('iot-data', region_name='ap-northeast-1')
+
+def lambda_handler(event, context):
+    print(event)
+    
+    # Push E-Mail
+    params = {
+        'TopicArn': 'arn:aws:sns:ap-northeast-1:1234567890:GeofenceEvent',
+        'Subject': 'GNS Tracker Event',
+        'Message': json.dumps(event, indent=2)
+    }
+    sns.publish(**params)
+    
+    # Publish IoT Core
+    try:
+        event_type = event['detail']['EventType']
+        pub_msg =  {
+            'event': event_type
+        }
+        iot.publish(
+            topic = 'core2/gps/notify',
+            qos = 0,
+            payload = json.dumps(pub_msg)
+        )
+    except Exception as e:
+        print(e)
+        raise e
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
